a2df72ec49f9418fa8819def91f905bca55afb97
JonathanN <JonathanN@nextgen.nextgen.net>
2018-02-08 16:54:49 +1100

    Commit becaus eno battery on laptop
    Commiting just in case the computers dies forever

--- api/controllers/propertyController.js
+++ api/controllers/propertyController.js
@@ -1,13 +1,25 @@
 const Property = require('../models/property');
 const Error = require('../models/error');
 
-const parseAsync = Promise.(JSON.parse);
+
+const filter = (payload) => {
+  if (!payload || !Array.isArray(payload)) return;
+  const response = [];
+  for (const property in payload) {
+    const filteredProperty = property.filter();
+    if(filteredProperty) response.push(filteredProperty);
+  }
+}
 
 exports.filterProperties = (payload) => {
   Promise
     .resolve(payload)
     .then(JSON.parse)
+    .then((json) => {
+    })
     .catch((error) => {
-      return { "error": "Could node decode request: JSON parsing failed" };
+      return {
+        "error": "Could node decode request: JSON parsing failed"
+      };
     })
 }

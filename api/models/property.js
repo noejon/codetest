a1c8af66fe9bebc4c579c4a14789434ff7ee0c97
JonathanN <JonathanN@nextgen.nextgen.net>
2018-02-12 15:27:44 +1100

    Refactored Property and Address class
    Getter and Setters removed from the Property and Address classes

--- api/models/property.js
+++ api/models/property.js
@@ -1,74 +1,26 @@
 const Address = require('./address');
 
 class Property {
-  set address(address) {
-    this._address = new Address(address);
-  }
-  get address() {
-    return this._address;
-  }
-  set propertyTypeId(propertyTypeId) {
-    this._propertyTypeId = propertyTypeId;
-  }
-  get propertyTypeId() {
-    return this._propertyTypeId;
-  }
-  set readyState(readyState) {
-    this._readyState = readyState;
-  }
-  get readyState() {
-    return this._readyState;
-  }
-  set reference(reference) {
-    this._reference = reference;
-  }
-  get reference() {
-    return this._reference;
-  }
-  set shortId(shortId) {
-    this._shortId = shortId;
-  }
-  get shortId() {
-    return this._shortId;
-  }
-  set status(status) {
-    this._status = status;
-  }
-  get status() {
-    return this._status;
-  }
-  set type(type) {
-    this._type = type;
-  }
-  get type() {
-    return this._type;
-  }
-  set workflow(workflow) {
-    this._workflow = workflow;
-  }
-  get workflow() {
-    return this._workflow;
-  }
 
   constructor(params) {
     if (!params) return;
-    this._address = new Address(params.address);
-    this._propertyTypeId = params.propertyTypeId;
-    this._readyState = params.readyState;
-    this._reference = params.reference;
-    this._shortId = params.shortId;
-    this._status = params.status;
-    this._type = params.type;
-    this._workflow = params.workflow;
+    this.address = new Address(params.address);
+    this.propertyTypeId = params.propertyTypeId;
+    this.readyState = params.readyState;
+    this.reference = params.reference;
+    this.shortId = params.shortId;
+    this.status = params.status;
+    this.type = params.type;
+    this.workflow = params.workflow;
   }
 
   filter() {
-    if (this._workflow === 'completed' &&
-      this._type === 'htv') {
+    if (this.workflow === 'completed' &&
+      this.type === 'htv') {
       return {
-        concatAddress: this._address.concat(),
-        type: this._type,
-        workflow: this._workflow
+        concatAddress: this.address.concat(),
+        type: this.type,
+        workflow: this.workflow
       };
     }
   }
--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express'),
   app = express(),
   port = process.env.PORT || 3000,
-  propertyController = require('./api/controllers/propertyController');
+  bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+/**
+ * Check if there is an error with the request
+ */
+app.use((err, req, res, next) => {
+  if(err) res.json({error: "Could not decode request: JSON parsing failed"});
+  else next();
+})
 
 const routes = require('./api/routes/propertyRoutes'); //importing route
 routes(app); //register the route
 
 app.listen(port);
-
-console.log('Codetest server has been started on port: ' + port);
